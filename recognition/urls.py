e94a56d40313c9cae1411ff08400efd1ebfba6f7
Njoro90260 <wchegesalome@gmail.com>
2025-05-28 21:01:43 +0300

    setup recognition app.

---
+++ recognition/urls.py
@@ -0,0 +1,10 @@
+"""Defines url patterns for the recognition app."""
+
+from django.urls import path
+from . import views
+
+app_name = 'recognition'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
--- recognition/views.py
+++ recognition/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    context = {
+        'title': 'FaceTrack lite App', 
+        'message': 'Welcome to the Recognition App!'
+        }
+    return render(request, 'recognition/index.html', context)
