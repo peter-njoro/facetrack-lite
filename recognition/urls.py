d7468332e06a7a1eccf14256b4b6f97bf73b9312
peter-njoro <wchegesalome@gmail.com>
2025-06-04 18:55:52 +0300

    started live recognition feed.

--- recognition/urls.py
+++ recognition/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('enroll/success', views.enroll_success, name='enroll_success'),
     path('detect/', views.face_detection_page, name='face_detection'),
     path('video_feed/', views.face_detection_stream, name='video_feed'),
+    path('live/', views.live_recognition_page, name='live_recognition'),
+    path('live_feed', views.live_recognition_stream, name='live_feed'),
 ]
